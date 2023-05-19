90f162f0897a02cd357d1955da4f91fb61d07d6e
Stacey29 <Kastacey@yandex.ru>
2023-05-19 22:42:49 +0300

    Initial commit

---
+++ src/main/java/com/example/dogandcat/DogAndCatApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogandcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogAndCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogAndCatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dogandcat/DogAndCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogandcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogAndCatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
